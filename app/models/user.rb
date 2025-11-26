b13b1735110174d64e002eb3ba0be704be0351c5
Tasuku Koni <koni.tasuku@gmail.com>
2025-11-26 17:46:36 +0900

    worked on unread notice. However, it is still work in progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   has_many :subscriptions, dependent: :destroy
   validates :role, presence: true
   belongs_to :school, optional: true
+  has_many :read_notifications
+  has_many :read_notices, through: :read_notifications, source: :notice
 
   def notices
     grades = self.subscriptions.pluck(:grade) + ["All"]
