a13520a56a63309360991d7f16b6333637a925da
Tasuku Koni <koni.tasuku@gmail.com>
2025-12-02 10:57:31 +0900

    worked on notes new create

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,5 @@
 class NotesController < ApplicationController
+  before_action :set_student, only: [:new, :create]
 
   def index
     @notes = Note.all
@@ -9,14 +10,30 @@ class NotesController < ApplicationController
   end
 
   def new
-
+    @note = @student.notes.new
   end
 
   def create
-
+    @note = @student.notes.new(note_params)
+    if @note.save
+      redirect_to note_path(@note), notice:"Message was successfully created"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
 
   end
+
+  private
+
+  def set_student
+    @student = Student.find(params[:student_id])
+  end
+
+  def note_params
+    params.require(:note).permit(:title, :content, :reply)
+  end
+
 end
