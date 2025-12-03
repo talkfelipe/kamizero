a31e0e5a5ccd5e444edd46c71f619fc430b9e0af
Tasuku Koni <koni.tasuku@gmail.com>
2025-12-03 14:41:29 +0900

    worked on unread note reply

--- app/models/read_notification.rb
+++ app/models/read_notification.rb
@@ -1,5 +1,6 @@
 class ReadNotification < ApplicationRecord
   belongs_to :user
   belongs_to :notification, polymorphic: true
-  validates :status, presence: true
+  validates :status, inclusion: { in: [true,false] }
+  # validate presence true made it difficult to make the reply unread
 end
